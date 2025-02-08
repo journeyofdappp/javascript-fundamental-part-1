ce256181a1116fb26319a540827a7489e50de0de
Dafa Alvin <dafaalvin10@gmail.com>
2025-02-08 23:00:54 +0700

    equality and identity

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-const money = 0;
+const number = Number(prompt("Enter a number"));
 
-if (money) {
-  console.log("Don't spend it all");
-} else {
-  console.log("You should get a job!");
-}
-
-let height;
-if (height) {
-  console.log("Yay! Height is defined");
-} else {
-  console.log("Height is undefined");
+if (number == 23) {
+  console.log("Equality 23");
 }
