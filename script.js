9bf63eebd985bdeeb76343fe4cf3ab298cc78b56
Dafa Alvin <dafaalvin10@gmail.com>
2025-02-09 14:18:38 +0700

    function declaration and expression

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-const number = Math.floor(Math.random() * 10) + 1;
-console.log(number);
+"use strict";
 
-console.log(
-  number > 6 ? "Number is greater than 6🧑‍💻" : "Number is less than 6😊"
-);
+// Function declaration
+function calculateAge(birthYear) {
+  return 2025 - birthYear;
+}
+
+const myAge = calculateAge(2005);
+console.log(myAge);
+
+// Function expression
+const message = function (firstName, message) {
+  return `Hi ${firstName}, ${message}`;
+};
+
+const messageData = message("Sara", "How are you?");
+console.log(messageData);
+
+// Arrow function
+const calculateAgeArrow = (firstName, birthYear) => {
+  const age = 2025 - birthYear;
+  const retirement = 65 - age;
+  const retirementYears = retirement + 2025;
+
+  return `${firstName}'s age is ${age} years, she retires in ${retirementYears}`;
+};
+console.log(calculateAgeArrow("Sarah", 2005));
