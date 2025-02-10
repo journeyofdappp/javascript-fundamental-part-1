a6d08e31457dec720b6a5a2c458a822b8c5b53b5
Dafa Alvin <dafaalvin10@gmail.com>
2025-02-10 18:57:25 +0700

    for loop

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-const mark = {
-  fullName: "Mark Miller",
-  mass: 78,
-  height: 1.69,
-  calcBMI: function () {
-    this.bmi = this.mass / (this.height * this.height);
-    return this.bmi;
-  },
-};
+const yearBirth = [2009, 2005, 1990, 2000, 1998, 1993, 1999];
+const ages = [];
 
-const john = {
-  fullName: "John Smith",
-  mass: 92,
-  height: 1.95,
-  calcBMI: function () {
-    this.bmi = this.mass / (this.height * this.height);
-    return this.bmi;
-  },
-};
+for (let i = 0; i <= yearBirth.length; i++) {
+  console.log(yearBirth[i]);
+  if (2024 - yearBirth[i] >= 18) {
+    ages.push(`Age: ${2024 - yearBirth[i]} (adult)`);
+  } else {
+    ages.push(`Age: ${2024 - yearBirth[i]} (minor)`);
+  }
+}
 
-console.log(mark.calcBMI());
-console.log(john.calcBMI());
+console.log(ages);
 
-if (mark.bmi > john.bmi) {
-  console.log(
-    `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`
-  );
-} else if (mark.bmi < john.bmi) {
-  console.log(
-    `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`
-  );
+const dummyData = ["Michael", 12, "John", 1, 2, 3];
+
+for (let data = 0; data < dummyData.length; data++) {
+  if (typeof dummyData[data] !== "string") {
+    continue;
+  }
+
+  console.log(dummyData[data]);
+}
+
+const dummyArr = [
+  "Jonas",
+  1991,
+  "teacher",
+  2018,
+  ["Michael", "Peter", "Steven"],
+];
+
+for (let exercise = 1; exercise <= 4; exercise++) {
+  console.log(`-`.repeat(30));
+  for (let rep = 1; rep <= 5; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weights repetition ${rep} 🏋️‍♀️`);
+  }
 }
