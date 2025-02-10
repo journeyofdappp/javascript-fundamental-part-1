c4bd3d94c7bd2224dc72ce618d8116c6f903aa70
Dafa Alvin <dafaalvin10@gmail.com>
2025-02-10 19:58:58 +0700

    looping's project

--- script.js
+++ script.js
@@ -1,38 +1,28 @@
-const yearBirth = [2009, 2005, 1990, 2000, 1998, 1993, 1999];
-const ages = [];
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
 
-for (let i = 0; i <= yearBirth.length; i++) {
-  console.log(yearBirth[i]);
-  if (2024 - yearBirth[i] >= 18) {
-    ages.push(`Age: ${2024 - yearBirth[i]} (adult)`);
-  } else {
-    ages.push(`Age: ${2024 - yearBirth[i]} (minor)`);
-  }
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
 }
 
-console.log(ages);
+console.log(tips);
+console.log(totals);
 
-const dummyData = ["Michael", 12, "John", 1, 2, 3];
-
-for (let data = 0; data < dummyData.length; data++) {
-  if (typeof dummyData[data] !== "string") {
-    continue;
+const calcAverage = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
   }
 
-  console.log(dummyData[data]);
-}
+  return sum / arr.length;
+};
 
-const dummyArr = [
-  "Jonas",
-  1991,
-  "teacher",
-  2018,
-  ["Michael", "Peter", "Steven"],
-];
-
-for (let exercise = 1; exercise <= 4; exercise++) {
-  console.log(`-`.repeat(30));
-  for (let rep = 1; rep <= 5; rep++) {
-    console.log(`Exercise ${exercise}: Lifting weights repetition ${rep} 🏋️‍♀️`);
-  }
-}
+console.log(`Bill average: ${calcAverage(bills)}`);
+console.log(`Tips average: ${calcAverage(tips)}`);
